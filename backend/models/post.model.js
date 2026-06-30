bdc7d49e6a9f0904027feddcd912515e125e2a37
Akash Mishra <akashmishra967033@gmail.com>
2026-06-30 18:51:45 +0530

    initial commit

---
+++ backend/models/post.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    userId:{
+
+    },
+    body:{
+
+    },
+    likes:{
+
+    },
+    createdAt:{
+
+    },
+    updateAt:{
+
+    },
+    media:{
+
+    },
+    active:{
+
+    },
+    fileType:{
+        
+    }
+
+})
