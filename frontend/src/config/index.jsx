4bad0f25b2c64ab114a8d4c62d10012eceab75a4
Akash Mishra <akashmishra967033@gmail.com>
2026-07-15 19:25:09 +0530

    add

---
+++ frontend/src/config/index.jsx
@@ -0,0 +1,6 @@
+const {default: axios} = require("axios");
+
+
+const clientServer = axios.create({
+    baseURL: "http://localhost:9090",
+})
---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,11 @@
+import {configureStore, isAction} from "@reduxjs/toolkit"
+
+
+// step for state management
+// submit Action
+// handle action in its reducer 
+// register here => Redux
+
+export const store = configureStorte({
+    reducer:{}
+})
