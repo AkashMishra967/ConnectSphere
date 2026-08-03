f00bb12b9472d97376bc5695c19e98f6469d58ad
Akash Mishra <akashmishra967033@gmail.com>
2026-08-03 19:33:51 +0530

    add

---
+++ frontend/app/discover/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DashboardLayout from "@/src/layouts/DashboardLayouts";
+import Userlayouts from "@/src/layouts/userlayouts";
+export default function DiscoverPage() {
+  return (
+     <Userlayouts>
+    <DashboardLayout>
+
+  <h1>Discover Page</h1>
+
+
+    </DashboardLayout>
+    </Userlayouts>
+  )
+}
---
+++ frontend/app/my-conections/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DashboardLayout from "@/src/layouts/DashboardLayouts";
+import Userlayouts from "@/src/layouts/userlayouts";
+export default function MyConnectionPage() {
+  return (
+    <Userlayouts>
+    <DashboardLayout>
+
+  <h1>My Connections</h1>
+
+
+    </DashboardLayout>
+    </Userlayouts>
+  )
+}
