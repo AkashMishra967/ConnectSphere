1cd51a4b7589fe7e74b2bdd78dbbdd5826d71158
Akash Mishra <akashmishra967033@gmail.com>
2026-08-19 17:57:38 +0530

    add

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,7 +2,7 @@ const {default: axios} = require("axios");
 
 
 
-export const BASE_URL = "http://localhost:9090"
+export const BASE_URL = "https://connectsphere-8vzz.onrender.com"
 
 const clientServer = axios.create({
   baseURL: BASE_URL,
